98881f7a6568799f38f28f19cce785774c539de2
Muramono <CDDumbacher@gmail.com>
2020-10-07 21:49:43 -0500

    Green Light |

--- RCube/create.py
+++ RCube/create.py
@@ -2,5 +2,6 @@ import hashlib
 
 def _create(parms):
     faces = '111111111222222222333333333444444444555555555666666666'
-    result = {'cube': faces}
+    Sha256 = hashlib.sha256(faces).hexdigest()
+    result = {'cube': faces, 'integrity': Sha256}
     return result
