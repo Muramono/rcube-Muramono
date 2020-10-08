5960b0f8323a86eccd371b12fbf5ffa1d884c9e8
Muramono <CDDumbacher@gmail.com>
2020-10-07 22:20:38 -0500

    Refactor |

--- RCube/create.py
+++ RCube/create.py
@@ -5,9 +5,12 @@ def _create(parms):
     cube = ""
     for i in faces:
         for x in range(9):
-            cube += str(x)
+            cube += str(i)
+    #print(cube)
     cube = bytearray(cube,'utf8')
     Sha256 = hashlib.sha256(cube).hexdigest()
     Sha256 = Sha256.upper()
     result = {'cube': faces, 'integrity': Sha256}
     return result
+
+_create({'op': 'create', 'faces': '123457'})
