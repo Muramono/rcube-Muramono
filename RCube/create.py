0944478b3b7ece8a0f43a06c31428442e60c6199
Muramono <CDDumbacher@gmail.com>
2020-10-07 21:52:54 -0500

    Refactor |

--- RCube/create.py
+++ RCube/create.py
@@ -1,7 +1,7 @@
 import hashlib
 
 def _create(parms):
-    faces = '111111111222222222333333333444444444555555555666666666'
-    Sha256 = hashlib.sha256(faces).hexdigest()
+    faces = "111111111222222222333333333444444444555555555666666666"
+    Sha256 = hashlib.sha256(b"111111111222222222333333333444444444555555555666666666").hexdigest()
     result = {'cube': faces, 'integrity': Sha256}
     return result
