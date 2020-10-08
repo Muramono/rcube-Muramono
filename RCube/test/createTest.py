4cf8fe61b1a21b32e1d376dcf4226c57f0e5c244
Muramono <CDDumbacher@gmail.com>
2020-10-07 21:57:08 -0500

    Green Light |

--- RCube/test/createTest.py
+++ RCube/test/createTest.py
@@ -9,11 +9,11 @@ import RCube.create as create
 class createTest(unittest.TestCase):
 
 
-    def test100_010_ShouldRetrunCubeWithFaces(self):
-        expectedResult = {'cube': '111111111222222222333333333444444444555555555666666666'}
-        parms = {'op': 'create'}
-        actualResult = create._create(parms)
-        self.assertDictEqual(expectedResult, actualResult)
+#     def test100_010_ShouldRetrunCubeWithFaces(self):
+#         expectedResult = {'cube': '111111111222222222333333333444444444555555555666666666'}
+#         parms = {'op': 'create'}
+#         actualResult = create._create(parms)
+#         self.assertDictEqual(expectedResult, actualResult)
 
     def test100_020_ShouldRetrunCubeWithSHA(self):
         expectedResult = {'cube': '111111111222222222333333333444444444555555555666666666', 'integrity': '88D897BD22E132D21A538745E63995B07D7C52CE9617A0979520545753EE0DED'}
