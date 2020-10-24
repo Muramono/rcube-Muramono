5b50007bd82c373678314974e9a857704a2c08de
Muramono <CDDumbacher@gmail.com>
2020-10-24 00:38:59 -0500

    Refactor |

--- RCube/check.py
+++ RCube/check.py
@@ -3,10 +3,10 @@ import hashlib
 def _check(parms):
     status = ''
     firstSpot = ''
-    midSpot = parms['cube'][4]
+    midSpot = ''
     counter = 0
     FullCheck = True
-    SpotCheck = True
+    SpotCheck = False
     midIndexs = [4,13,18,23,28,33]
     ByteCube = bytearray(parms['cube'],'utf8')
     IntegrityKey = hashlib.sha256(ByteCube).hexdigest()
@@ -17,10 +17,12 @@ def _check(parms):
         if(counter == 0 or counter % 9 == 0):
             firstSpot = spot
         if(counter in midIndexs):
+            print("Spot Check")
             midSpot = spot
-        if(spot == midSpot):
-            SpotCheck = False
+        if(spot != midSpot):
+            SpotCheck = True
         if(spot != firstSpot):
+            print("Full Check")
             FullCheck = False
         counter += 1
     if(FullCheck == True):
@@ -29,3 +31,5 @@ def _check(parms):
         status = 'spots'
     result = {'status': status}
     return result
+
+_check({'op': 'check', 'cube': 'rrrrbrrrryyyyryyyyoooogoooowwwwowwwwbbbbby', 'integrity': '103D576CB789BDAE083FBD95A05B5829009C465E388F9A8CFAD60B87CF6DD21A'})
