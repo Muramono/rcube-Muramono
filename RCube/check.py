997db843ee5410d500a000f2f0cb27b4122f2d69
Muramono <CDDumbacher@gmail.com>
2020-10-24 18:29:02 -0500

    Refactor |

--- RCube/check.py
+++ RCube/check.py
@@ -7,7 +7,7 @@ def _check(parms):
     cornerSpot = ''
     counter = 0
     FullCheck = True
-    SpotCheck = False
+    SpotCheck = True
     CrossCheck = True
     ByteCube = bytearray(parms['cube'],'utf8')
     IntegrityKey = hashlib.sha256(ByteCube).hexdigest()
@@ -15,18 +15,27 @@ def _check(parms):
     if(parms['integrity'] != IntegrityKey):
         return {'status': 'error bad integrity key'}
     for spot in parms['cube']:
+        #Sets Tracked spots to the appropriate value of the current face
         if(counter == 0 or counter % 9 == 0):
             firstSpot = spot
             midSpot = parms['cube'][counter + 4]
             cornerSpot = spot
-        if(( counter + 1) % 2 != 0 and (counter + 1) % 5 != 0):
+        #Checks to see if the odd and not the middle spot are equal to the corner spot(s) **CROSS CHECK**
+        if( ( counter + 1) % 2 != 0 and (counter + 1) % 5 != 0):
             if(spot != cornerSpot):
                 CrossCheck = False
+        #Checks if the even spots or middle spot is equal to the middle **CROSS CHECK**
         if( (counter + 1) % 2 == 0 and (counter + 1) % 5 == 0):
             if(spot != midSpot):
                 CrossCheck = False
-        if(spot != midSpot):
-            SpotCheck = True
+        #Checks if the middle spot is equal to any outer spots **SPOTS CHECK**
+        if( (counter + 1) % 5 != 0):
+            if(spot == midSpot):
+                SpotCheck = False
+        if( (counter + 1) % 5 == 0):
+            if(spot != firstSpot):
+                SpotCheck = False
+        #Checks to see if any spot is different **FULL CHECK**
         if(spot != firstSpot):
             FullCheck = False
         counter += 1
