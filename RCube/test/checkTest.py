fe7869f1c3c652ae27eb557fe08c8eea1643bc23
Muramono <CDDumbacher@gmail.com>
2020-10-23 22:16:08 -0500

    Red Light |

--- RCube/test/checkTest.py
+++ RCube/test/checkTest.py
@@ -15,8 +15,13 @@ class checkTest(unittest.TestCase):
         parms = {'op': 'check'}
         actualResult = check._check(parms)
         self.assertDictEqual(expectedResult, actualResult)
-    def test000_010_ShouldTakeCubeParmas(self):
+    def test000_020_ShouldTakeCubeParmas(self):
         expectedResult = {'check': 'check stub'}
         parms = {'op': 'check', 'cube': 'gggggggggyyyyyyyyybbbbbbbbbwwwwwwwwwrrrrrrrrrooooooooo'}
+        actualResult = check._check(parms)
+        self.assertDictEqual(expectedResult, actualResult)
+    def test000_030_SimpleFullCheck(self):
+        expectedResult = {'status': 'full'}
+        parms = {'op': 'check', 'cube': 'gggggggggyyyyyyyyybbbbbbbbbwwwwwwwwwrrrrrrrrrooooooooo', 'integrity': '763F71B164EF77E6916F1C2CBAEB3B2C3CA9A876AC6A94A97D6B0EF1C489E289'}
         actualResult = check._check(parms)
         self.assertDictEqual(expectedResult, actualResult)
