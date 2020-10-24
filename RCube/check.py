514996693d5cbd38face37dc128f3e32bfcf33bd
Muramono <CDDumbacher@gmail.com>
2020-10-24 00:58:04 -0500

    Green Light |

--- RCube/check.py
+++ RCube/check.py
@@ -4,25 +4,23 @@ def _check(parms):
     status = ''
     firstSpot = ''
     midSpot = parms['cube'][4]
+    print(midSpot)
     counter = 0
     FullCheck = True
     SpotCheck = False
-    midIndexs = [4,13,18,23,28,33]
     ByteCube = bytearray(parms['cube'],'utf8')
     IntegrityKey = hashlib.sha256(ByteCube).hexdigest()
     IntegrityKey = IntegrityKey.upper()
     if(parms['integrity'] != IntegrityKey):
         return {'status': 'error bad integrity key'}
     for spot in parms['cube']:
+        print(spot)
         if(counter == 0 or counter % 9 == 0):
             firstSpot = spot
-        if(counter in midIndexs):
-            print("Spot Check")
-            midSpot = spot
+            midSpot = parms['cube'][counter + 4]
         if(spot != midSpot):
             SpotCheck = True
         if(spot != firstSpot):
-            print("Full Check")
             FullCheck = False
         counter += 1
     if(FullCheck == True):
