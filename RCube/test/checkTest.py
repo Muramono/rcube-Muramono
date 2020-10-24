acb9d328cb42f5f0876e83aef0564eb03895690a
Muramono <CDDumbacher@gmail.com>
2020-10-23 22:08:31 -0500

    Red Light |

--- RCube/test/checkTest.py
+++ RCube/test/checkTest.py
@@ -10,8 +10,13 @@ class checkTest(unittest.TestCase):
     
     ## Iteration 2 incremental development test
     
-        def test000_010_ShouldReturnDefaultStatus(self):
-            expectedResult = {'check': 'check stub'}
-            parms = {'op': 'check'}
-            actualResult = check._check(parms)
-            self.assertDictEqual(expectedResult, actualResult)
+    def test000_010_ShouldReturnDefaultStatus(self):
+        expectedResult = {'check': 'check stub'}
+        parms = {'op': 'check'}
+        actualResult = check._check(parms)
+        self.assertDictEqual(expectedResult, actualResult)
+    def test000_010_ShouldTakeCubeParmas(self):
+        expectedResult = {'check': 'check stub'}
+        parms = {'op': 'check', 'cube': 'gggggggggyyyyyyyyybbbbbbbbbwwwwwwwwwrrrrrrrrrooooooooo'}
+        actualResult = check._check(parms)
+        self.assertDictEqual(expectedResult, actualResult)
