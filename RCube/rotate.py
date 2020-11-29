683c4a77bb34f4c655937eca301912409391b104
Muramono <CDDumbacher@gmail.com>
2020-11-28 19:36:41 -0600

    Red Light |

--- RCube/rotate.py
+++ RCube/rotate.py
@@ -1,5 +1,39 @@
 def _rotate(parms):
-    result = {'rotate': 'rotate stub'}
+    result = dict()
+    status = ''
+    cube = ''
+    integritykey = ''
+    
+    result = {'status': status, 'cube': cube, 'integrity': integritykey}
+    
     return result
 
 ## Idea Transpose left and right side to get columns then swap them itertively 
+def FrontFaceRotate():
+    
+##END
+
+def RightFaceRotate():
+    
+##END
+
+def BackFaceRotate():
+    
+##END
+
+def TopFaceRotate(): 
+    
+##END
+
+def BottomFaceRotate():
+    
+##END
+
+def IntegrityCheck():
+
+##END
+
+def CubeValidation():
+    
+##END
+
--- RCube/test/rotateTest.py
+++ RCube/test/rotateTest.py
@@ -10,11 +10,11 @@ class rotateTest(unittest.TestCase):
 
     ## Iteration 3 incremental pre-product development test
      
-    def test000_010_ShouldReturnDefaultStatus(self):
-        expectedResult = {'rotate': 'rotate stub'}
-        parms = {'op': 'rotate'}
-        actualResult = rotate._rotate(parms)
-        self.assertDictEqual(expectedResult, actualResult)
+#     def test000_010_ShouldReturnDefaultStatus(self):
+#         expectedResult = {'rotate': 'rotate stub'}
+#         parms = {'op': 'rotate'}
+#         actualResult = rotate._rotate(parms)
+#         self.assertDictEqual(expectedResult, actualResult)
         
     def test000_020_ShouldReturnCubeStatusIntegrity(self):
         expectedResult = {'status': 'rotated', 'cube': 'ggggggggg rrrrrrrrr bbbbbbbbb ooooooooo wwwwwwwww yyyyyyyyy', 'integrity': '546F560EB2D04BAA5F0F0EBB2F74EF9B0EC42B5EF005E2418B69671DAD467FCF'}
