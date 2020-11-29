bf7f83b4371b592fac9ba3d76bf9fed8e87dd31e
Muramono <CDDumbacher@gmail.com>
2020-11-28 23:20:54 -0600

    Green Light |

--- RCube/rotate.py
+++ RCube/rotate.py
@@ -256,30 +256,30 @@ def LeftFaceRotate(parms):
         return Separator.join(cube)
     if(parms['side'] == 'L'):
         cube = list(parms['cube'])
-        #Left Side Values That Change
-        LeftS = cube[27] + cube[30] + cube[33]
+        #Back Side Values That Change
+        BackS = cube[20] + cube[23] + cube[26]
         #Bottom Side Values That Change
-        BotS = cube[51] + cube[52] + cube[53]
-        #Right Side Values That Change
-        RightS = cube[11] + cube[14] + cube[17]
+        BotS = cube[45] + cube[48] + cube[51]
+        #Front Side Values That Change
+        FrontS = cube[0] + cube[3] + cube[6]
         #Top Side Values That Change
-        TopS = cube[36] + cube[37] + cube[38]
-        #Set Left Side
-        cube[27] = BotS[0]
-        cube[30] = BotS[1]
-        cube[33] = BotS[2]
+        TopS = cube[36] + cube[39] + cube[42]
+        #Set Back Side
+        cube[20] = TopS[2]
+        cube[23] = TopS[1]
+        cube[26] = TopS[0]
         #Set Bottom
-        cube[51] = RightS[2]
-        cube[52] = RightS[1]
-        cube[53] = RightS[0]
-        #Set Right
-        cube[11] = TopS[0]
-        cube[14] = TopS[1]
-        cube[17] = TopS[2]
+        cube[45] = BackS[2]
+        cube[48] = BackS[1]
+        cube[51] = BackS[0]
+        #Set Front
+        cube[0] = BotS[0]
+        cube[3] = BotS[1]
+        cube[6] = BotS[2]
         #Set Top
-        cube[36] = LeftS[1]
-        cube[37] = LeftS[1]
-        cube[38] = LeftS[2]
+        cube[36] = FrontS[0]
+        cube[39] = FrontS[1]
+        cube[42] = FrontS[2]
         #Concatenating List Back Together
         Separator = ''
         return Separator.join(cube)
