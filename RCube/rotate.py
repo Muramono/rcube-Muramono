c4f7aa4808ec618103943279840ee5c69104f957
Muramono <CDDumbacher@gmail.com>
2020-11-28 19:40:32 -0600

    Yellow Light |

--- RCube/rotate.py
+++ RCube/rotate.py
@@ -4,8 +4,11 @@ def _rotate(parms):
     cube = ''
     integritykey = ''
     
-    result = {'status': status, 'cube': cube, 'integrity': integritykey}
+    cube = parms['cube']
+    status = 'rotate'
+    integritykey = '546F560EB2D04BAA5F0F0EBB2F74EF9B0EC42B5EF005E2418B69671DAD467FCF'
     
+    result = {'status': status, 'cube': cube, 'integrity': integritykey}
     return result
 
 ## Idea Transpose left and right side to get columns then swap them itertively 
