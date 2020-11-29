f990e1414a78cef0a70e20d0427c80e731480ca5
Muramono <CDDumbacher@gmail.com>
2020-11-28 20:23:20 -0600

    Green Light |

--- RCube/test/rotateTest.py
+++ RCube/test/rotateTest.py
@@ -17,7 +17,7 @@ class rotateTest(unittest.TestCase):
 #         self.assertDictEqual(expectedResult, actualResult)
         
     def test000_020_ShouldReturnCubeStatusIntegrity(self):
-        expectedResult = {'status': 'rotated', 'cube': 'ggggggggg rrrrrrrrr bbbbbbbbb ooooooooo wwwwwwwww yyyyyyyyy', 'integrity': '546F560EB2D04BAA5F0F0EBB2F74EF9B0EC42B5EF005E2418B69671DAD467FCF'}
+        expectedResult = {'status': 'rotated', 'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy', 'integrity': '546F560EB2D04BAA5F0F0EBB2F74EF9B0EC42B5EF005E2418B69671DAD467FCF'}
         parms = {'op': 'rotate', 'side': 'F', 'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy', 'integrity': '546F560EB2D04BAA5F0F0EBB2F74EF9B0EC42B5EF005E2418B69671DAD467FCF'}
         actualResult = rotate._rotate(parms)
         self.assertDictEqual(expectedResult, actualResult)
