6f0d670da3d62f472ead20cbd1b59035ee91771d
Muramono <CDDumbacher@gmail.com>
2020-11-28 22:07:54 -0600

    Refactor |

--- RCube/test/rotateTest.py
+++ RCube/test/rotateTest.py
@@ -29,7 +29,7 @@ class rotateTest(unittest.TestCase):
         actualResult = rotate._rotate(parms)
         self.assertDictEqual(expectedResult, actualResult)
     def test100_020_FrontCounterClockwiseCheck(self):
-        expectedResult = {'status': 'rotated', 'cube': 'ggggggggg yrryrryrr bbbbbbbbb oowoowoow wwwwwwrrr oooyyyyyy', 'integrity': '546F560EB2D04BAA5F0F0EBB2F74EF9B0EC42B5EF005E2418B69671DAD467FCF'}
+        expectedResult = {'status': 'rotated', 'cube': 'gggggggggyrryrryrrbbbbbbbbboowoowoowwwwwwwrrroooyyyyyy', 'integrity': '546F560EB2D04BAA5F0F0EBB2F74EF9B0EC42B5EF005E2418B69671DAD467FCF'}
         parms = {'op': 'rotate', 'side': 'F', 'cube': 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy', 'integrity': '546F560EB2D04BAA5F0F0EBB2F74EF9B0EC42B5EF005E2418B69671DAD467FCF'}
         actualResult = rotate._rotate(parms)
         self.assertDictEqual(expectedResult, actualResult)
